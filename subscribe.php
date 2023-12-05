56ac516dd0cc6e21623a19511abe6243b0e0505f
Josie-Kwan <jkwan31@gatech.edu>
2023-12-04 20:01:24 -0500

    trying again

--- subscribe.php
+++ subscribe.php
@@ -1,35 +1,31 @@
 <?php
 
-// Include library if needed
-require_once 'path/to/your/library.php';
-
 // Get email from form
 $email = $_POST['email'];
 
 // Validate email address (optional)
 if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-  die("Invalid email address!");
+  echo '<span style="color: red;">Please enter a valid email address.</span>';
+  exit; // Stops script execution after showing the error message
 }
 
-// Create connection to spreadsheet (replace with your details)
-$spreadsheet_url = "https://docs.google.com/spreadsheets/d/YOUR_SPREADSHEET_ID/edit";
-$spreadsheet_id = "1eM8uxmBCpDw2uuhfiuTGrpbx-oqcFuO7SY-Y9me3xgY";
-$sheet_name = "Sheet1"; // Change this if needed
 
-// Open spreadsheet
-$spreadsheet = new Spreadsheet($spreadsheet_url);
+$spreadsheet_url = "https://docs.google.com/spreadsheets/d/1eM8uxmBCpDw2uuhfiuTGrpbx-oqcFuO7SY-Y9me3xgY";
+$sheet_name = "Sheet1"; 
+
+$spreadsheet = new Spreadsheet_Excel_Reader($spreadsheet_url);
 
 // Get sheet
-$sheet = $spreadsheet->getActiveSheet();
+$sheet = $spreadsheet->sheets[0];
 
 // Find next empty row
-$row_number = $sheet->getHighestRow() + 1;
+$row_number = count($sheet['cells']) + 1; // Assumes starting from row 1
 
 // Set email in spreadsheet
-$sheet->setCellValueByColumnAndRow(1, $row_number, $email);
+$sheet['cells'][$row_number][1] = $email;
 
 // Save spreadsheet
-$spreadsheet->updateCells("$sheet_name!A$row_number:$row_number");
+$spreadsheet->writeXLS($spreadsheet_url); // Replace with actual saving method if using a library
 
 // Success message
 echo "Thank you for subscribing! Your email has been added.";
